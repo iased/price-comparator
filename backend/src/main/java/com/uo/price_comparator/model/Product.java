cc6c109d52161754b360ae6309ea342c54fbd890
iased <ias.edina@gmail.com>
2025-11-05 14:27:04 +0200

    Add JPA entities for Product, Supermarket, ProductPrice, and Discount

---
+++ backend/src/main/java/com/uo/price_comparator/model/Product.java
@@ -0,0 +1,80 @@
+package com.uo.price_comparator.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String brand;
+    private String category;
+    private double quantity;
+    private String unit;
+
+    public Product(){
+
+    }
+
+    public Product(Long id, String name, String brand, String category, double quantity, String unit) {
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.category = category;
+        this.quantity = quantity;
+        this.unit = unit;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+}
